b5688cc2c5c963c31e0d50d87fc4caf76d697bb4
yclvergil <yclvergil@gmail.com>
2017-04-23 17:02:47 +0800

    Initial commit

---
+++ e3managerinterface/src/main/java/cn/e3mall/service/ItemService.java
@@ -0,0 +1,11 @@
+package cn.e3mall.service;
+
+import cn.e3mall.pojo.TbItem;
+
+/**
+ * Created by cl on 2017/4/23.
+ */
+public interface ItemService {
+
+    TbItem getItemById(long itemId);
+}
---
+++ e3managerservice/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package cn.e3mall.service.impl;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by cl on 2017/4/23.
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        //根据主键查询
+        TbItem item = itemMapper.selectByPrimaryKey(itemId);
+        return item;
+    }
+}
